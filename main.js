5f3d1050f154e4e125be05903d08812afaf835a2
OctavianDobre8 <octavian.dobre@student.unitbv.ro>
2024-04-25 23:55:38 +0300

    basic scoreboard
    i built a basic scoreboard implementation, now i have to modify the things in it to make it to the final product

---
+++ main.js
@@ -0,0 +1,22 @@
+import ScoreboardView from './Scoreboard/ScoreboardView.js';
+
+let playerOneScore = 0;
+let playerTwoScore = 0;
+const root = document.getElementById('app');
+const view = new ScoreboardView(
+  root,
+  'Player One',
+  'Player Two',
+  (player, direction) => {
+    //update the score
+    const difference = direction === 'minus' ? -1 : 1;
+
+    if (player === 'one') {
+      playerOneScore = Math.max(playerOneScore + difference, 0);
+    } else {
+      playerTwoScore = Math.max(playerTwoScore + difference, 0);
+    }
+
+    view.update(playerOneScore, playerTwoScore);
+  }
+);
